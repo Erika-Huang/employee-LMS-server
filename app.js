e78431b9ecda1398d84a5bc9d01bd6691e3e1aaf
Erika Huang <suiic@qq.com>
2023-09-04 10:10:28 +0800

    fix: 修复了删除单个用户时会退出登录

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ require('./config/db')
 
 // middlewares
 app.use(bodyparser({
-  enableTypes:['json', 'form', 'text']
+  enableTypes: ['json', 'form', 'text']
 }))
 app.use(json())
 app.use(logger())
@@ -33,14 +33,14 @@ app.use(views(__dirname + '/views', {
 // 中间件的执行优先于接口
 // 例如登录的时候一定会先加载中间件，先获取参数，然后执行next，如果没有中间件才执行登录接口
 app.use(async (ctx, next) => {
-  log4js.info(`post params:${JSON.stringify(ctx.request.query)}`)
-  log4js.info(`get params:${JSON.stringify(ctx.request.body)}`)
-  await next().catch((err)=>{
-    if(err.status == '401') { // koa-jwt把500报错改为401
-      ctx.status = 200
-      ctx.body = util.fail('Token认证失败',util.CODE.AUTH_ERROR) // 接口业务码
-    }else{
-      throw err
+  log4js.info(`get params:${JSON.stringify(ctx.request.query)}`)
+  log4js.info(`post params:${JSON.stringify(ctx.request.body)}`)
+  await next().catch((err) => {
+    if (err.status == '401') { // koa-jwt把500报错改为401
+      ctx.status = 200;
+      ctx.body = util.fail('Token认证失败', util.CODE.AUTH_ERROR) // 接口业务码
+    } else {
+      throw err;
     }
   })
 })
@@ -48,15 +48,15 @@ app.use(async (ctx, next) => {
 // 任何一个接口进来都会先经过这个过滤一遍，来校验token是否有效
 // koa-jwt本身是基于jsonwebtoken做一层拦截，decoded
 app.use(koajwt({
-  secret:'employee-leave-management-system'
+  secret: 'employee-leave-management-system'
 }).unless({
-  path:[/^\/api\/users\/login/] // 过滤掉登录接口，不需要验证token
+  path: [/^\/api\/users\/login/] // 过滤掉登录接口，不需要验证token
 })) // 401
 
 // routes
 router.prefix('/api')
 
-router.get('/leave/count',(ctx)=> {
+router.get('/leave/count', (ctx) => {
   console.log('=>', ctx.request.headers);
   const token = ctx.request.headers.authorization.split(' ')[1]
   // 验证，解密数据
@@ -64,7 +64,7 @@ router.get('/leave/count',(ctx)=> {
   ctx.body = payload
 })
 
-router.use(users.routes(),users.allowedMethods())
+router.use(users.routes(), users.allowedMethods())
 app.use(router.routes(), router.allowedMethods())
 
 // error-handling
--- utils/log4j.js
+++ utils/log4j.js
@@ -1,6 +1,6 @@
 /**
  * 日志存储
- * @author Erika
+ * @author JackBean
  */
 const log4js = require('log4js')
 
@@ -13,62 +13,59 @@ const levels = {
     'fatal': log4js.levels.FATAL,
 }
 
-// 配置
 log4js.configure({
-    // 追加器
     appenders: {
         console: { type: 'console' },
         info: {
             type: 'file',
-            filename: "logs/all-logs.log" 
+            filename: 'logs/all-logs.log'
         },
         error: {
             type: 'dateFile',
             filename: 'logs/log',
-            // 规则
             pattern: 'yyyy-MM-dd.log',
-            alwaysIncludePattern: true  // 设置文件名称为filename + pattern
+            alwaysIncludePattern: true// 设置文件名称为 filename + pattern
         }
     },
     categories: {
-        default: {  appenders: ["console"], level: "debug" },
+        default: { appenders: ['console'], level: 'debug' },
         info: {
-            appenders: ['info','console'],
+            appenders: ['info', 'console'],
             level: 'info'
         },
         error: {
-            appenders: ['error','console'],
+            appenders: ['error', 'console'],
             level: 'error'
         }
     }
 })
 
 /**
- * 日志输出，level 为 degug
+ * 日志输出，level为debug
  * @param {string} content 
  */
-exports.debug = (content)=> {
-    let logger = log4js.getLogger()
-    logger.level = levels.debug
-    logger.debug(content)
+exports.debug = (content) => {
+    let logger = log4js.getLogger();
+    logger.level = levels.debug;
+    logger.debug(content);
 }
 
 /**
- * 日志输出，level 为 info
+ * 日志输出，level为info
  * @param {string} content 
  */
-exports.info = (content)=> {
-    let logger = log4js.getLogger('info')
-    logger.level = levels.info
-    logger.info(content)
+exports.info = (content) => {
+    let logger = log4js.getLogger('info');
+    logger.level = levels.info;
+    logger.info(content);
 }
 
 /**
- * 日志输出，level 为 error
+ * 日志输出，level为error
  * @param {string} content 
  */
-exports.error = (content)=> {
-    let logger = log4js.getLogger('error')
-    logger.level = levels.error
-    logger.error(content)
+exports.error = (content) => {
+    let logger = log4js.getLogger('error');
+    logger.level = levels.error;
+    logger.error(content);
 }
