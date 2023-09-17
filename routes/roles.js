71bb16e2af214807c3768d8a9756a4f0c85dd372
Erika Huang <suiic@qq.com>
2023-09-17 13:47:34 +0800

    feat: 查询所有角色列表的名称和id接口/roles/alllist

--- routes/roles.js
+++ routes/roles.js
@@ -9,6 +9,15 @@ const md5 = require('md5')
 
 router.prefix('/roles')  // 二级路由前缀
 
+// 查询所有的角色列表 的名称和id
+router.get('/alllist', async (ctx) => {
+    try {
+        const list = await Role.find({}, "_id roleName")
+        ctx.body = util.success(list)
+    } catch (error) {
+        ctx.body = util.fail(`查询失败:${error.stack}`)
+    }
+})
 
 
 
